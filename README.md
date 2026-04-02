e1b3b8334c160fa7493cf76aebb3fb3143c20513
Samuel Fredric Berg <samuelberg.private@gmail.com>
2026-04-03 00:09:43 +0200

    feat: Added mod_overrides

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 - [SuperBLT](https://superblt.znix.xyz/)
 - [BeardLib](https://modworkshop.net/mod/14924)
+
+# NOTE:
+
+The mod_overrides/ folder should not be in here but within the assets folder of game files.
--- inventory-body-bags/code.lua
+++ inventory-body-bags/code.lua
@@ -1,10 +1,11 @@
-local tweaker = PlayerTweakData.init
-function PlayerTweakData:init(tweak_data)
-    tweaker(self, tweak_data)
+local old_ibb_ptd_init = PlayerTweakData.init
 
-    -- Sets max carrying capacity
-    self.body_bags.max_amount = 99
+function PlayerTweakData:init(tweak_data)
+    old_ibb_ptd_init(self, tweak_data)
+
+	-- Sets max carrying capacity
+	self.body_bags.max_amount = 99
     
     -- Sets how many you spawn with
-    self.body_bags.start_amount = 10
+	self.body_bags.start_amount = 10
 end
