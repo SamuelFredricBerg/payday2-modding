747a246ef9101a9d68f8149653108073297b3a75
Samuel Fredric Berg <samuelberg.private@gmail.com>
2026-04-04 19:00:24 +0200

    chore: Some clean up

--- mods/meth-helper/code.lua
+++ mods/meth-helper/code.lua
@@ -16,20 +16,21 @@ ingredient_dialog["pln_rat_stage1_28"] = "Meth is done"
 local _queue_dialog_orig = DialogManager.queue_dialog
 function DialogManager:queue_dialog(id, ...)
     if ingredient_dialog[id] == last_ingredient then
-	ingredient_said = true
+	    ingredient_said = true
     else
-	ingredient_said = false
+	    ingredient_said = false
     end
     if ingredient_dialog[id] and toggle_ingredients_chat and not ingredient_said then
-	managers.chat:send_message(ChatManager.GAME, managers.network.account:username() or "Offline", ingredient_dialog[id]) --Comment this out to stop messages being sent in chat
-	last_ingredient = ingredient_dialog[id]
+        managers.chat:send_message(ChatManager.GAME, managers.network.account:username() or "Offline", ingredient_dialog[id]) --Comment this out to stop messages being sent in chat
+        last_ingredient = ingredient_dialog[id]
     elseif ingredient_dialog[id] and not toggle_ingredients_chat then
-	managers.hud:show_hint({text = ingredient_dialog[id]})
-	last_ingredient = ingredient_dialog[id]
+        managers.hud:show_hint({text = ingredient_dialog[id]})
+        last_ingredient = ingredient_dialog[id]
     end
     if ingredient_dialog[id] == "Add muriatic acid" or ingredient_dialog[id] == "Add caustic soda" or ingredient_dialog[id] == "Add hydrogen chloride" then
-	ingredient_said = true
+	    ingredient_said = true
     end
+
     return _queue_dialog_orig(self, id, ...)
 end
 
